317c6365aa9f2437d30e74c0f0ba286e6813d7ec
hasanasadov <hasanaliasadov@gmail.com>
2025-07-10 13:56:02 +0400

    envs added

--- front-end/src/services/axiosInstance.ts
+++ front-end/src/services/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: import.meta.env.VITE_APP_API_BASE_URL || "http://localhost:4000",
   withCredentials: true,
 });
 
